e006117052203a0e9a9a0fc856bd208733ea90c1
DariaZaiakina <daasha94@gmail.com>
2020-10-27 15:47:42 +0300

    Добавлена функция поиска мин. и макс. значений в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,18 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(',').join(' ').split(' ');
+  let arr2 = [];
+
+   for (let i=0; i<arr.length; i++) {
+     if (!isNaN(arr[i])) {
+         arr2.push(arr[i]); 
+     }   
+   }
+ let result = {
+	 min: Math.min.apply(null, arr2),
+	 max: Math.max.apply(null, arr2)
+ }
+
+return result;
 }
