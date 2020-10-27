2f6fb12dc8459231b795da13ca81dc63f8fdc271
DariaZaiakina <daasha94@gmail.com>
2020-10-27 21:31:17 +0300

    Добавлена функция showSalary

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -5,5 +5,13 @@
  * @returns {string}
  */
 function showSalary(users, age) {
-  // ваш код...
+  let res = [];
+  for (let i=0; i<users.length; i++){
+    if (users[i].age <= age)
+    {
+      res.push(`${users[i].name}, ${users[i].balance}`);      
+    }
+    
+  }  
+return (res.join('\n'));
 }
