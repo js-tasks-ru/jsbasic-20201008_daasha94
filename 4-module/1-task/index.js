83e4dd6dbfe69a01698044ee72083f8c9040e20e
DariaZaiakina <daasha94@gmail.com>
2020-10-29 23:47:46 +0300

    Добавлена функция, преобразующая массив в стандартный HTML список

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,11 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  for(let i = 0; i<friends.length; i++)
+  {
+    ul.appendChild(document.createElement('li'));
+    ul.children[i].append(friends[i].firstName + " " + friends[i].lastName);   
+  } 
+  return ul;
 }
